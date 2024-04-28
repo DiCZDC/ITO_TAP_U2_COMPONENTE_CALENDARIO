af007d661099563555345d90d294d4a6d0a25902
Menma1928 <168324082+Menma1928@users.noreply.github.com>
2024-04-28 16:05:02 -0600

    Add files via upload

---
+++ misClases/src/prueba/probarContrasenia.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package prueba;
+
+/**
+ *
+ * @author raulc
+ */
+public class probarContrasenia {
+    
+    public static boolean probarLongitudMinima(String texto, int Lminima){
+        return texto.length() >= Lminima;
+    }
+    
+    public static boolean probarLongitudMaxima(String texto, int Lmaxima){
+        return texto.length()<= Lmaxima;
+    }
+    
+    public static boolean probarTieneMayusculas(String texto){
+        return !texto.equals(texto.toLowerCase());
+    }
+    
+    public static boolean probarTieneNumeros(String texto){
+        for (int i = 0; i < texto.length(); i++) {
+            if (Character.isDigit(texto.charAt(i))) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public static boolean probarCaracteresEspeciales(String texto){
+        for (int i = 0; i < texto.length(); i++) {
+            char caracter = texto.charAt(i);
+            if (!Character.isLetterOrDigit(caracter)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    
+}
